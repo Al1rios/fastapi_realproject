1baf0773c2040be35139a73b9ba243d9124e9ca7
Ali Rios <ali.rios@gmail.com>
2022-07-28 06:11:05 -0300

    correction bug js

--- public/index.js
+++ public/index.js
@@ -10,7 +10,12 @@ async function load_animals(){
 
     animals.forEach(animal => {
         const item = document.createElement('li')
-        item.innerText = animal.name
+        
+        const line = `${animal.name} - age: ${animal.age} - color: ${animal.color}`
+
+
+
+        item.innerText = line
 
         list_one.appendChild(item)
     });
@@ -19,19 +24,21 @@ async function load_animals(){
 }
 
 function management_form(){
-    const form_animal = document.getElementById('list_animals')
-
+    const form_animal = document.getElementById('form_animals')
     const input_name = document.getElementById('name')
 
     form_animal.onsubmit = async (event) => {
         event.preventDefault()
         const name_animal = input_name.value
+
+
         await axios.post('http://localhost:8000/animals', {
             name: name_animal,
             age: 12,
             sex: 'female',
             color: 'brown'
         })
+
         load_animals()
         alert('Animal added success')
     }
