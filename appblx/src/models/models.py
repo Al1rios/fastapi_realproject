a47e1da6fec7e650c29e5747a8b3a55ce02a5e6c
Ali Rios <ali.rios@gmail.com>
2022-07-30 13:14:42 -0300

    added project blx with schema

---
+++ appblx/src/models/models.py
@@ -0,0 +1,33 @@
+from matplotlib.style import available
+from pydantic import BaseModel
+from typing import Optional, List
+
+class User(BaseModel):
+    id: Optional[str] = None
+    name: str
+    phone: str
+    my_products: List[Product]
+    my_sales: List[PurchaseOrder]
+    my_purcharse_order: List[PurchaseOrder]
+
+class Product(BaseModel):
+    id: Optional[str] = None
+    user: User
+    name: str
+    details: str
+    price: float
+    available: bool = False
+
+class PurchaseOrder(BaseModel):
+    id: Optional[str] = None
+    user: User
+    product: Product
+    amount: int
+    delivery: bool = True
+    address: str
+    comment: Optional[str] = "no comment"
+
+
+
+
+
